8a942610749fce1f01f3076fc381e5d5464af78d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-01-04 21:57:48 +0000

    mv

--- diapyr/test_diapyr.py
+++ diapyr/test_diapyr.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with diapyr.  If not, see <http://www.gnu.org/licenses/>.
 
+from . import DI, types
 import unittest
-from diapyr import DI, types
 
 class TestDI(unittest.TestCase):
 
