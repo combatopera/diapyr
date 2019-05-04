254c294500da94d194f58c2717c5b587f4431a53
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-05-04 10:33:47 +0100

    innerclass decorator

---
+++ diapyr/util.py
@@ -0,0 +1,32 @@
+# Copyright 2014, 2018 Andrzej Cichocki
+
+# This file is part of diapyr.
+#
+# diapyr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diapyr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diapyr.  If not, see <http://www.gnu.org/licenses/>.
+
+def innerclass(cls):
+    def getproxy(enclosinginstance):
+        class Inner(cls):
+            def __getattr__(self, name):
+                try:
+                    return getattr(enclosinginstance, name)
+                except AttributeError:
+                    superclass = super(Inner, self)
+                    try:
+                        supergetattr = superclass.__getattr__
+                    except AttributeError:
+                        raise AttributeError("'%s' object has no attribute '%s'" % (cls.__name__, name))
+                    return supergetattr(name)
+        return Inner
+    return property(getproxy)
