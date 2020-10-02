49b86e133ac702ad5977a2dd9e47cf427babc220
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-10-02 20:53:52 +0100

    todo

--- diapyr/iface.py
+++ diapyr/iface.py
@@ -21,6 +21,7 @@ class Special(object):
 
     @classmethod
     def gettypelabel(cls, type):
+        # TODO: Dotted name can look misleading for nested classes.
         return type.__name__ if issubclass(type, cls) else "%s.%s" % (type.__module__, type.__name__)
 
 class UnsatisfiableRequestException(Exception): pass
