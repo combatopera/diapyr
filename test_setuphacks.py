5795c88a15286fad010b0cca2acf0bc7a5fcf22e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-03-22 11:31:50 +0000

    migrating

--- test_setuphacks.py
+++ test_setuphacks.py
@@ -1,19 +1,19 @@
-# Copyright 2013, 2014, 2015, 2016, 2017, 2020, 2022 Andrzej Cichocki
+# Copyright 2014, 2018, 2019, 2020, 2024 Andrzej Cichocki
 
-# This file is part of pyven.
+# This file is part of diapyr.
 #
-# pyven is free software: you can redistribute it and/or modify
+# diapyr is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pyven is distributed in the hope that it will be useful,
+# diapyr is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyven.  If not, see <http://www.gnu.org/licenses/>.
+# along with diapyr.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuphacks import getsetupkwargs
 from tempfile import NamedTemporaryFile
